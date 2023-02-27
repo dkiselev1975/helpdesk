6125f98a2dded5c8dc0cc9c5a2075983ab45ff30
Дмитрий Киселев <dkiselev2000@mail.ru>
2023-02-27 11:11:02 +0300

    Использована основная .css из frontend. Созданы .css для frontend

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,6 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/default.css',
         'css/site.css',
     ];
     public $js = [
