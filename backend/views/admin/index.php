d2e8c39ece26fe1b86164b9cea8d99755551ed79
Дмитрий Киселев <dkiselev2000@mail.ru>
2023-08-15 17:22:52 +0300

    json static data output

--- backend/views/admin/index.php
+++ backend/views/admin/index.php
@@ -2,7 +2,12 @@
 /** @var yii\web\View $this */
 /** @var array $items */
 /** @var string $empty_list_phrase */
+/** @var object $users */
 
 $this->title = Yii::$app->params['app_name']['backend'];
-
+foreach ($users->users as $v)
+    {
+    echo $v->last_name."|".$v->jobtitle."<br>";
+    }
+?><pre><?php var_dump($users);?></pre><?php
 ?>
