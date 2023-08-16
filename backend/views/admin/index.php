a04a91931e1982b2f5cb13a7b17180201a4cdc96
Дмитрий Киселев <dkiselev2000@mail.ru>
2023-08-16 16:12:10 +0300

    selected fields in table

--- backend/views/admin/index.php
+++ backend/views/admin/index.php
@@ -8,25 +8,40 @@ $this->title = Yii::$app->params['app_name']['backend'];
 var_dump (count($users->users));
 
 $level=0;
+$fields=array('name','jobtitle','login_name','first_name','middle_name','last_name','department');
+
 function get_value(string $key,mixed $value,&$level)
     {
     $level++;
-    if($level==1){?><td><?}
-    ?><ul><?
+    if($level<=1){$tag="td";}else{$tag="ul";}
+    echo "<".$tag.">";
     if(!is_object($value))
         {
-        ?><li><?=$key.": ".$value."[".$level."]";?></li><?
+        if($tag==='td')
+            {
+            echo $key.": ".$value." [".$level."]";
+            }
+        else
+            {
+            ?><li><?=$key.": ".$value." [".$level."]";?></li><?
+            }
         }
     else
         {
-        ?><li><?=$key."(is_obj)"."[".$level."]";?></li><?
+        if($tag==='td')
+            {
+            echo $key." (is_obj)"." [".$level."]";
+            }
+        else
+            {
+            ?><li><?=$key." (is_obj)"." [".$level."]";?></li><?
+            }
         foreach ($value as $k=>$v)
             {
             get_value($k,$v,$level);
             }
         }
-    ?></ul><?
-    if($level==1){?></td><?}
+    echo "</".$tag.">";
     --$level;
     }
 
@@ -36,9 +51,12 @@ if(count($users->users)>0)
     foreach ($users->users as $k=>$v)
         {
         $level=0;
-        ?><tr><?php
-        get_value($k,$v,$level);
-        ?></tr><?php
+        ?><tr><?
+            foreach($fields as $key)
+                {
+                get_value($key,$v->$key,$level);
+                }
+        ?></tr><?
         }
     ?></table><?php
     }
