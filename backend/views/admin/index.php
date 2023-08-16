1e4700b7fd3909cfb0dc4a1dd818920fe337ab30
Дмитрий Киселев <dkiselev2000@mail.ru>
2023-08-16 13:48:56 +0300

    data object tree

--- backend/views/admin/index.php
+++ backend/views/admin/index.php
@@ -5,9 +5,46 @@
 /** @var object $users */
 
 $this->title = Yii::$app->params['app_name']['backend'];
-foreach ($users->users as $v)
+var_dump (count($users->users));
+
+$level=0;
+function get_value(string $key,mixed $value,&$level)
     {
-    echo $v->last_name."|".$v->jobtitle."<br>";
+    $level++;
+    if($level==1){?><td><?}
+    ?><ul><?
+    if(!is_object($value))
+        {
+        ?><li><?=$key.": ".$value."[".$level."]";?></li><?
+        }
+    else
+        {
+        ?><li><?=$key."(is_obj)"."[".$level."]";?></li><?
+        foreach ($value as $k=>$v)
+            {
+            get_value($k,$v,$level);
+            }
+        }
+    ?></ul><?
+    if($level==1){?></td><?}
+    --$level;
     }
-?><pre><?php var_dump($users);?></pre><?php
+
+if(count($users->users)>0)
+    {
+    ?><table class="table table-bordered table-striped data-table"><?php
+    foreach ($users->users as $k=>$v)
+        {
+        $level=0;
+        ?><tr><?php
+        get_value($k,$v,$level);
+        ?></tr><?php
+        }
+    ?></table><?php
+    }
+
+
+?><pre><?php var_dump($users->users);?></pre><?php
+
+
 ?>
