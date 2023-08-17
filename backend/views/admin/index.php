7840898d76a9e687d231f84fbb07b2e6e91e355f
Дмитрий Киселев <dkiselev2000@mail.ru>
2023-08-17 15:53:48 +0300

    class structure implemented

--- backend/views/admin/index.php
+++ backend/views/admin/index.php
@@ -7,12 +7,11 @@
 $this->title = Yii::$app->params['app_name']['backend'];
 ?><p><strong>Count: </strong><?var_dump (count($users->users));?></p><?
 
-$level=0;
-$fields=array('name','jobtitle','phone','department');
-
-function get_value(string $key,mixed $value,string $section,int &$level)
-    {
-    $classes=array(
+class showTree
+{
+    private $fields=array('name','jobtitle','phone','department');
+    private array $classes=
+    [
         'primary'=>
             [
                 0=>
@@ -45,60 +44,67 @@ function get_value(string $key,mixed $value,string $section,int &$level)
                         'is_scalar_value'=>'secondary_value fw-normal',
                     ],
             ],
-    );
-    if(is_object($value))
+    ];
+
+    function get_value(string $key,mixed $value,string $section)
+    {
+        static $level=0;
+
+        if(is_object($value))
         {
-        $class=$classes[$section][(int)((bool)$level)]['is_object'];
-        ?><div class="<?=$class;?> ps-<?=$level?>"><?
-        ?><div class="<?=$classes[$section][(int)((bool)$level)]['object_title']?>" title="(is_obj),level: <?=$level;?>"><?=$key.":";?></div><?
-        foreach ($value as $k=>$v)
+            $class=$this->classes[$section][(int)((bool)$level)]['is_object'];
+            ?><div class="<?=$class;?> ps-<?=$level?>"><?
+            ?><div class="<?=$this->classes[$section][(int)((bool)$level)]['object_title']?>" title="(is_obj),level: <?=$level;?>"><?=$key.":";?></div><?
+            foreach ($value as $k=>$v)
             {
-            $level++;
-            get_value($k,$v,$section,$level);
-            --$level;
+                $level++;
+                $this->get_value($k,$v,$section);
+                --$level;
             }
-        ?></div><?
+            ?></div><?
         }
-    else
+        else
         {
-        $class=$classes[$section][(int)((bool)$level)]['is_scalar_value'];
-        ?><div class="<?=$class;?> ps-<?=$level?>" title="level: <?=$level;?>"><?=$key.": ".(is_null($value)?'<span class="text-secondary">Нет данных</span>':$value);?></div><?
+            $class=$this->classes[$section][(int)((bool)$level)]['is_scalar_value'];
+            ?><div class="<?=$class;?> ps-<?=$level?>" title="level: <?=$level;?>"><?=$key.": ".(is_null($value)?'<span class="text-secondary">Нет данных</span>':$value);?></div><?
         }
     }
-
-if(count($users->users)>0)
+    function makeTree($data)
     {
-    ?><div class="tree_container border-light"><?php
-    foreach ($users->users as $k=>$v)
+        if(count($data)>0)
         {
-        ?><div class="item_container border p-2 my-4"><?
-            //var_dump(array_diff($v,$fields));
-            $level=0;
-            ?><div class="primary_fields_container w-100 fw-bolder d-flex flex-wrap pb-2 bg-light"><?
-                foreach($fields as $key)
-                    {
-                    if(property_exists($v,$key))
-                        {
-                        get_value($key,$v->$key,'primary',$level);
-                        }
-                    }
-            ?></div><?
-            ?><div class="secondary_fields_container w-100 fw-normal d-flex flex-wrap pb-3 border-top"><?
-            foreach ($v as $second_key=>$second_value)
+            ?><div class="tree_container border-light"><?php
+            foreach ($data as $k=>$v)
+            {
+                ?><div class="item_container border p-2 my-4"><?
+                //var_dump(array_diff($v,$this->fields));
+                ?><div class="primary_fields_container w-100 fw-bolder d-flex flex-wrap pb-2 bg-light"><?
+                foreach($this->fields as $key)
                 {
-                if(!in_array($second_key,$fields))
+                    if(property_exists($v,$key))
                     {
-                    get_value($second_key,$v->$second_key,'secondary',$level);
+                        $this->get_value($key,$v->$key,'primary');
                     }
                 }
-            ?></div><?
-        ?></div><?
+                ?></div><?
+                ?><div class="secondary_fields_container w-100 fw-normal d-flex flex-wrap pb-3 border-top"><?
+                foreach ($v as $second_key=>$second_value)
+                {
+                    if(!in_array($second_key,$this->fields))
+                    {
+                        $this->get_value($second_key,$v->$second_key,'secondary');
+                    }
+                }
+                ?></div><?
+                ?></div><?
+            }
+            ?></div><?php
         }
-    ?></div><?php
     }
+}
 
+$tree=new showTree();
+$tree->makeTree($users->users);
 
 ?><pre><?php var_dump($users->users);?></pre><?php
-
-
 ?>
